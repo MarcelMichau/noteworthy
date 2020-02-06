ce6606639370441abcabbb916c43d3b606c25440
Marcel Michau <marcelmichau@gmail.com>
2020-02-06 06:03:37 +0200

    Updates

---
+++ precache-manifest.a59f348c35bd0ba056f1519af32fed39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46e71573a4c980650370e2610328b69",
+    "url": "/spotiview/index.html"
+  },
+  {
+    "revision": "f8e84cdf104e3dc772c5",
+    "url": "/spotiview/static/css/main.152b8c17.chunk.css"
+  },
+  {
+    "revision": "20434565195a3d10bf62",
+    "url": "/spotiview/static/js/2.3bad37ce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/spotiview/static/js/2.3bad37ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e84cdf104e3dc772c5",
+    "url": "/spotiview/static/js/main.51750de1.chunk.js"
+  },
+  {
+    "revision": "cdab0ac9f6853c64a5b7",
+    "url": "/spotiview/static/js/runtime-main.495e99d4.js"
+  }
+]);
