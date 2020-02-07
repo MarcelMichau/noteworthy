e3d6f799c8323ffacf746e907203eeafa0bfd4c6
Marcel Michau <Marcel.Michau@investec.co.za>
2020-02-07 07:25:35 +0200

    Updates

---
+++ precache-manifest.ab06951dd013d7404e0120046a2a52bb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e762cac37b865c947455e2481c3e312",
+    "url": "/noteworthy/index.html"
+  },
+  {
+    "revision": "a2055243186ab6102b0e",
+    "url": "/noteworthy/static/css/2.4f5bb317.chunk.css"
+  },
+  {
+    "revision": "a2055243186ab6102b0e",
+    "url": "/noteworthy/static/js/2.e2f4cdf5.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/noteworthy/static/js/2.e2f4cdf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0763ad2dfe7e458f6664",
+    "url": "/noteworthy/static/js/main.1aed42cc.chunk.js"
+  },
+  {
+    "revision": "305a0fb383f8b978c498",
+    "url": "/noteworthy/static/js/runtime-main.a1326f64.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/noteworthy/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/noteworthy/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/noteworthy/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/noteworthy/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/noteworthy/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/noteworthy/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/noteworthy/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/noteworthy/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/noteworthy/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/noteworthy/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/noteworthy/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/noteworthy/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/noteworthy/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/noteworthy/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/noteworthy/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/noteworthy/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotiview/precache-manifest.b37f93e8c49c6040c02f24990754a96f.js"
+  "/noteworthy/precache-manifest.ab06951dd013d7404e0120046a2a52bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotiview/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noteworthy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
